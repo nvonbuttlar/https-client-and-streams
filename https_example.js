a5dab0ec3d51edf4fa2f4e2b71401125e891e615
vonbutt <nick.von.buttlar@gmail.com>
2018-10-01 06:11:50 +0000

    not the response we were seeking

--- https_example.js
+++ https_example.js
@@ -11,8 +11,9 @@ var options = {
 
 // Called by https when the request is made.
 
-var callback = function(){
+var callback = function(response){
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
