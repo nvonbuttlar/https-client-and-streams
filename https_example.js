1f7ff3c6ed703f5e5b9fe1cd7031e9148a1c8f16
vonbutt <nick.von.buttlar@gmail.com>
2018-10-01 06:20:28 +0000

    proper response in callback fn

--- https_example.js
+++ https_example.js
@@ -13,7 +13,11 @@ var options = {
 
 var callback = function(response){
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk){
+    console.log('[-- LENGTH OF CHUNK: ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
